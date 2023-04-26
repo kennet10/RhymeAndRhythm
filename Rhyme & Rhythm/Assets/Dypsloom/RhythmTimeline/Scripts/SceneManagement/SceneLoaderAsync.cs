ff426c98be4f4180c5481128fe4f6fb2a66adb97
267thao <267thao@gmail.com>
2023-04-26 16:03:42 -0700

    added basic song selection scene with async scene loader

---
+++ Rhyme & Rhythm/Assets/Dypsloom/RhythmTimeline/Scripts/SceneManagement/SceneLoaderAsync.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Dypsloom.RhythmTimeline.SceneManagement
+{
+    public class SceneLoaderAsync : MonoBehaviour
+    {
+        [SerializeField] protected int m_SceneBuildIndex;
+
+        [ContextMenu("LoadScene")]
+        public void LoadSceneAsync()
+        {
+            LoadSceneAsync(m_SceneBuildIndex);
+        }
+
+        private void LoadSceneAsync(int buildIndex)
+        {
+            SceneManager.LoadSceneAsync(buildIndex);
+        }
+    }
+}
