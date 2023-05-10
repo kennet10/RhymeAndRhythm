eeb67da7c97b61e436022df15095f8a453ee227f
Waeng <92623587+kennet10@users.noreply.github.com>
2023-05-09 21:11:54 -0700

    added pause menus

--- Rhyme & Rhythm/Assets/UIManager.cs
+++ Rhyme & Rhythm/Assets/UIManager.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.UI;
 
-public class Score : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     [Tooltip("The Playable Director.")]
     [SerializeField] protected PlayableDirector m_PlayableDirector;
-    [SerializeField] protected GameObject m_ScorePanel;
+
+    [SerializeField] protected GameObject m_ScoreMenu;
     [SerializeField] protected TMPro.TextMeshProUGUI m_ScoreText;
     [SerializeField] protected TMPro.TextMeshProUGUI m_ComboText;
     [SerializeField] protected TMPro.TextMeshProUGUI m_ScoreDisplay;
     [SerializeField] protected TMPro.TextMeshProUGUI m_ComboDisplay;
+
+    [SerializeField] protected GameObject m_PauseMenu;
+
+    private bool m_GameIsPaused;
 
     // Start is called before the first frame update
     void Awake()
     {
         m_PlayableDirector.timeUpdateMode = DirectorUpdateMode.DSPClock;
         m_PlayableDirector.stopped += HandleSongEnded;
+
+        m_GameIsPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!m_GameIsPaused)
+            {
+                m_PauseMenu.SetActive(true);
+                m_PlayableDirector.Pause();
+                m_GameIsPaused = true;
+            }
+            else
+            {
+                m_PauseMenu.SetActive(false);
+                m_PlayableDirector.Resume();
+                m_GameIsPaused = false;
+            }
+        }
     }
 
     protected void HandleSongEnded(PlayableDirector playableDirector)
@@ -30,9 +56,9 @@ public class Score : MonoBehaviour
     {
         m_PlayableDirector.Stop();
 
-        if(m_ScorePanel != null)
+        if(m_ScoreMenu != null)
         {
-            m_ScorePanel.SetActive(true);
+            m_ScoreMenu.SetActive(true);
         }
 
         m_ScoreText.text = m_ScoreDisplay.text;
